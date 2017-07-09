3c29e8f7fa5a533e506a0f9a9a0568d64b06c07b
Marco Monteleone <mar.monteleone@gmail.com>
2017-07-09 19:59:56 +0200

    switched to neo4j OGM

--- src/main/java/models/Article.java
+++ src/main/java/models/Article.java
@@ -1,75 +1,30 @@
 package models;
 
 import java.net.URI;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
-public class Article {
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.Relationship;
+
+@NodeEntity(label="Paper")
+public class Article{	
+	@GraphId
+	private Long id;
+	@Property(name="title")
 	private String title;
+	@Property(name="year")
 	private String year;
+	@Property(name="venue")
 	private String journal;
+	@Property(name="abstract")
 	private String articleAbstract;
-	private URI articleUri;
-	private List<ArticleReference> references;
 	
-	public Article(String title, String year, String journal, String abs){
-		this.title = title;
-		this.year = year;
-		this.journal = journal;
-		this.articleAbstract = abs;
-		this.references = new LinkedList<>();
-	}
-
-	public String getTitle() {
-		return title;
-	}
-
-	public void setTitle(String title) {
-		this.title = title;
-	}
-
-	public String getYear() {
-		return year;
-	}
-
-	public void setYear(String year) {
-		this.year = year;
-	}
-
-	public String getJournal() {
-		return journal;
-	}
-
-	public void setJournal(String journal) {
-		this.journal = journal;
-	}
-
-	public String getArticleAbstract() {
-		return articleAbstract;
-	}
-
-	public void setArticleAbstract(String articleAbstract) {
-		this.articleAbstract = articleAbstract;
-	}
-
-	public URI getArticleUri() {
-		return articleUri;
-	}
-
-	public void setArticleUri(URI articleUri) {
-		this.articleUri = articleUri;
-	}
-
-	public List<ArticleReference> getReferences() {
-		return references;
-	}
-	
-	public void setReferences(List<ArticleReference> references) {
-		this.references = references;
-	}
-	
-	public void addReference(ArticleReference ref){
-		this.references.add(ref);
-	}	
+	@Relationship(type="PAPER_REFERENCE", direction="OUTGOING")
+	private Set<Article> references = new HashSet<>();
 	
 }
--- src/main/java/models/Author.java
+++ src/main/java/models/Author.java
@@ -3,47 +3,24 @@ package models;
 import java.util.LinkedList;
 import java.util.List;
 
-public class Author {
-	
+import org.neo4j.ogm.annotation.GraphId;
+import org.neo4j.ogm.annotation.NodeEntity;
+import org.neo4j.ogm.annotation.Property;
+import org.neo4j.ogm.annotation.Transient;
+import org.neo4j.ogm.annotation.typeconversion.Convert;
+
+@NodeEntity
+public class Author{
+	@GraphId
+	private Long id;
+	@Property(name="name")
 	private String name;
+	@Convert(InterestsConverter.class)
 	private List<String> interests;
+	@Transient
 	private List<Article> articles;
 
-	/*constructor for author without valid interests*/
-	public Author(String name, List<Article> articles){
-		this.name = name;
-		this.articles = articles;
-		this.interests = new LinkedList<>();
-	}
-	
-	/*constructor for author with valid interests*/
-	public Author(String name, List<Article> articles, List<String> interests){
-		this.name = name;
-		this.articles = articles;
-		this.interests = interests;
-	}	
-	
-	public String getName() {
-		return name;
-	}
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-	public List<String> getInterests() {
-		return interests;
-	}
-
-	public void setInterests(List<String> interests) {
-		this.interests = interests;
-	}
-
-	public List<Article> getArticles() {
-		return articles;
-	}
-
-	public void setArticles(List<Article> articles) {
-		this.articles = articles;
+	public void addArticle(Article article){
+		this.articles.add(article);
 	}
 }
