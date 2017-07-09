0cfbbed07059c4521523c4b4a52348e4815275f9
Marco Monteleone <mar.monteleone@gmail.com>
2017-07-09 20:32:00 +0200

    new version of Neo4jConnector

--- src/main/java/connectors/Neo4jConnector.java
+++ src/main/java/connectors/Neo4jConnector.java
@@ -5,55 +5,26 @@ import java.util.List;
 
 import models.Article;
 
-import org.neo4j.driver.v1.AuthTokens;
-import org.neo4j.driver.v1.Driver;
-import org.neo4j.driver.v1.GraphDatabase;
-import org.neo4j.driver.v1.Record;
-import org.neo4j.driver.v1.Session;
-import org.neo4j.driver.v1.StatementResult;
+import org.neo4j.ogm.session.Session;
+import org.neo4j.ogm.session.SessionFactory;
 
 public class Neo4jConnector {
-	private String host;
-	private String user;
-	private String password;
-	private Driver driver;
+	private static SessionFactory sessionFactory = new SessionFactory("models");
+	private Session session;
 	
-	public Neo4jConnector(String host, String user, String password){
-		this.host = host;
-		this.user = user;
-		this.password = password;
+	public Neo4jConnector(){
 	}
 	
-	public void connect(){
-		this.driver = GraphDatabase.driver(this.host, AuthTokens.basic(this.user, this.password));
-	}
-	
-    public void close(){
-        driver.close();
-    }
-	
+    /*Actually only gets 25 articles for now */
     public List<Article> getAllArticles(){
-    	List<Article> articles = new LinkedList<>();
+    	List<Article> articlesList = new LinkedList<>();
+    	this.session = sessionFactory.openSession();
+    	
+    	Iterable<Article> articles = session.loadAll(Article.class);
+    	for(Article article : articles)
+    		articlesList.add(article);
+    	
+    	return articlesList;
+    }    
 
-    	Session session = this.driver.session();
-
-    	StatementResult result = session.run("MATCH (n:Paper) "
-    			+ "RETURN n.title AS title, n.year AS year, n.venue AS venue, n.abstract AS abstract "
-    			+ "LIMIT 25");
-    	while (result.hasNext())
-    	{
-    		Record record = result.next();
-    		String title = record.get("title").asString();
-    		String year = record.get("year").asString();
-    		String journal = record.get("venue").asString();
-    		String abs = record.get("abstract").asString();
-    		if(abs == null)
-    			abs = "";
-    		Article article = new Article(title, year, journal, abs);
-    		System.out.println(title);
-    		articles.add(article);
-    	}
-
-    	return articles;
-    }
 }
